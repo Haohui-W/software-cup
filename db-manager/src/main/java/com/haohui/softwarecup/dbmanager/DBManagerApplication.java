da0e58605647864eddeccc4799cd603a2e5f0f52
Haohui <haohui-w@qq.com>
2022-05-31 16:52:22 +0800

    在主类当中调用service

---
+++ db-manager/src/main/java/com/haohui/softwarecup/dbmanager/DBManagerApplication.java
@@ -0,0 +1,21 @@
+package com.haohui.softwarecup.dbmanager;
+
+import com.haohui.softwarecup.dbmanager.service.DBInputZipFileService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+
+@SpringBootApplication
+public class DBManagerApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(DBManagerApplication.class, args);
+
+        DBInputZipFileService dbInputZipFileService = context.getBean(DBInputZipFileService.class);
+        try {
+            dbInputZipFileService.inputAndSave();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
