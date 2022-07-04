7c8de17b07136bf3c385b4d0e4d58482e0058780
Haohui <haohui-w@qq.com>
2022-07-04 19:42:46 +0800

    创建正式项目

---
+++ software-cup-api/src/main/java/com/haohui/softwarecup/softwarecupapi/SoftwareCupApiApplication.java
@@ -0,0 +1,11 @@
+package com.haohui.softwarecup.softwarecupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareCupApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareCupApiApplication.class,args);
+    }
+}
