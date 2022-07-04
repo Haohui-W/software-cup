95a7d3db6db70e71b34ff987f38107fa0276d7e1
Haohui <haohui-w@qq.com>
2022-07-04 19:41:46 +0800

    清理代码

--- news-api/src/main/java/com/haohui/softwarecup/newsapi/NewsApiApplication.java
+++ news-api/src/main/java/com/haohui/softwarecup/newsapi/NewsApiApplication.java
@@ -1,16 +1,11 @@
 package com.haohui.softwarecup.newsapi;
 
-import com.haohui.softwarecup.newsapi.service.Recommend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class NewsApiApplication {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(NewsApiApplication.class, args);
-//        Recommend bean = context.getBean(Recommend.class);
-//        bean.recommend();
-
+        SpringApplication.run(NewsApiApplication.class, args);
     }
 }
