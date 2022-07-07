a8b65e48294ce1257353e21a0d70bfe656b0e29e
Haohui <haohui-w@qq.com>
2022-07-08 03:09:21 +0800

    配置跨域

--- software-cup-api/src/main/java/com/haohui/softwarecup/softwarecupapi/SoftwareCupApiApplication.java
+++ software-cup-api/src/main/java/com/haohui/softwarecup/softwarecupapi/SoftwareCupApiApplication.java
@@ -2,10 +2,28 @@ package com.haohui.softwarecup.softwarecupapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class SoftwareCupApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SoftwareCupApiApplication.class,args);
+        SpringApplication.run(SoftwareCupApiApplication.class, args);
+    }
+    @Bean
+    @Order(0)
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource configSource
+                = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/api/**", config);
+        return new CorsWebFilter(configSource);
     }
 }
