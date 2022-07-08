f77f2e618b772f98d2d514a64a984ef29c688b4c
Haohui <haohui-w@qq.com>
2022-07-08 20:21:23 +0800

    提供新闻类型

--- news-api/src/main/java/com/haohui/softwarecup/newsapi/handler/APIHandler.java
+++ news-api/src/main/java/com/haohui/softwarecup/newsapi/handler/APIHandler.java
@@ -1,9 +1,12 @@
 package com.haohui.softwarecup.newsapi.handler;
 
+import com.haohui.softwarecup.newsapi.vo.ResultVO;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @RestController
 @CrossOrigin
@@ -23,7 +26,15 @@ public class APIHandler {
                 "获取给定时间之后的num个新闻<br />"+
                 "<br />"+
                 "/api/news/{type}/{num}<br />"+
-                "获取最多num条发布时间小于当前时间的对应类型的新闻";
+                "获取最多num条发布时间小于当前时间的对应类型的新闻"+
+                "<br />"+
+                "/api/newsType<br />"+
+                "获取可选的新闻类型"
+                ;
         return Mono.just(s);
+    }
+    @GetMapping("/api/newsType")
+    public Mono<ResultVO> getNewsType(){
+        return Mono.just(ResultVO.success(Map.of("newsType", NewsHandler.newsType)));
     }
 }
--- news-api/src/main/java/com/haohui/softwarecup/newsapi/handler/NewsHandler.java
+++ news-api/src/main/java/com/haohui/softwarecup/newsapi/handler/NewsHandler.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/news")
 @CrossOrigin
 public class NewsHandler {
-    private static final List<String> newsType = List.of("体育", "娱乐", "家居", "彩票", "房产", "教育", "时尚", "时政", "星座", "游戏", "社会", "科技", "股票", "财经");
+    public static final List<String> newsType = List.of("体育", "娱乐", "家居", "彩票", "房产", "教育", "时尚", "时政", "星座", "游戏", "社会", "科技", "股票", "财经");
 
     private final NewsDao newsDao;
 
