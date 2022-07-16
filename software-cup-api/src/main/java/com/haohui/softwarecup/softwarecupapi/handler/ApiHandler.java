8ed26db612cd5c28e3fedf6e3317b8576c256861
Haohui <haohui-w@qq.com>
2022-07-16 23:28:55 +0800

    配置ThymeleafView，并且将api文档直接重定向返回

--- software-cup-api/src/main/java/com/haohui/softwarecup/softwarecupapi/handler/ApiHandler.java
+++ software-cup-api/src/main/java/com/haohui/softwarecup/softwarecupapi/handler/ApiHandler.java
@@ -1,6 +1,7 @@
 package com.haohui.softwarecup.softwarecupapi.handler;
 
 import com.haohui.softwarecup.softwarecupapi.vo.ResultVO;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
@@ -10,17 +11,17 @@ import java.util.Map;
  * @author Haohui
  * 创建时间 2022/7/14/19:45
  */
-@RestController
+@Controller
 @CrossOrigin
 @RequestMapping("/api")
 public class ApiHandler {
     @GetMapping("/newsType")
+    @ResponseBody
     public Mono<ResultVO> getNewsType(){
         return Mono.just(ResultVO.ok(Map.of("newsType", NewsHandler.newsType)));
     }
     @GetMapping
     public Mono<String> getAPI(){
-        String res = "<a href=\"https://flowus.cn/haohui/share/7447cad4-bcd8-4dc4-b992-3a6982406b66\">api文档</a>";
-        return Mono.just(res);
+        return Mono.just("redirect:https://flowus.cn/haohui/share/7447cad4-bcd8-4dc4-b992-3a6982406b66");
     }
 }
