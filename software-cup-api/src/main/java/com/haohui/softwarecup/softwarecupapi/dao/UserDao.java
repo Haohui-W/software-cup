a885b47d5bd1cb4beaa59cbf37fb8d8911016a2c
Haohui <haohui-w@qq.com>
2022-07-24 22:00:37 +0800

    删除生日字段

--- software-cup-api/src/main/java/com/haohui/softwarecup/softwarecupapi/dao/UserDao.java
+++ software-cup-api/src/main/java/com/haohui/softwarecup/softwarecupapi/dao/UserDao.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 public interface UserDao extends R2dbcRepository<User,UUID > {
-    @Query("INSERT INTO software_cup_db.user(uuid, nick_name, username, user_password, key_words, birthday) values (:uuid,:nickName,:username,:userPassword,:keyWords,:birthday)")
+    @Query("INSERT INTO software_cup_db.user(uuid, nick_name, username, user_password, key_words, birthday) values (:uuid,:nickName,:username,:userPassword)")
     @Modifying
-    Mono<Integer> save(String uuid, String nickName, String username, String userPassword, String keyWords, LocalDate birthday);
+    Mono<Integer> save(String uuid, String nickName, String username, String userPassword, String keyWords);
 }
