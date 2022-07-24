e0c14559692897602ff65b4f989bceebc97cb4ca
Haohui <haohui-w@qq.com>
2022-07-24 21:58:52 +0800

    把fastjson转jackson

--- software-cup-api/src/main/java/com/haohui/softwarecup/softwarecupapi/service/UserService.java
+++ software-cup-api/src/main/java/com/haohui/softwarecup/softwarecupapi/service/UserService.java
@@ -8,4 +8,6 @@ import java.util.UUID;
 public interface UserService {
     Mono<Integer> save(User user);
     Mono<User> findById(UUID uuid);
+
+    Mono<Boolean> existUserByName(String username);
 }
--- software-cup-api/src/main/java/com/haohui/softwarecup/softwarecupapi/service/impl/UserServiceImpl.java
+++ software-cup-api/src/main/java/com/haohui/softwarecup/softwarecupapi/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.haohui.softwarecup.softwarecupapi.service.impl;
 
-import com.alibaba.fastjson2.JSON;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.haohui.softwarecup.softwarecupapi.dao.UserDao;
 import com.haohui.softwarecup.softwarecupapi.pojo.User;
 import com.haohui.softwarecup.softwarecupapi.service.UserService;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -13,25 +15,37 @@ import java.util.UUID;
 @Service
 public class UserServiceImpl implements UserService {
     private final UserDao userDao;
+    private final ObjectMapper objectMapper;
 
-    public UserServiceImpl(UserDao userDao) {
+    public UserServiceImpl(UserDao userDao, ObjectMapper objectMapper) {
         this.userDao = userDao;
+        this.objectMapper = objectMapper;
     }
 
     @Override
     public Mono<Integer> save(User user) {
-        return userDao.save(
-                user.getUuid().toString(),
-                user.getNickName(),
-                user.getUsername(),
-                user.getUserPassword(),
-                JSON.toJSONString(user.getKeyWords()==null?new ArrayList<String>():user.getKeyWords()),
-                user.getBirthDay()
-        );
+        try {
+            return userDao.save(
+                    user.getUuid().toString(),
+                    user.getNickName(),
+                    user.getUsername(),
+                    user.getUserPassword(),
+                    objectMapper.writeValueAsString(user.getKeyWords()==null?new ArrayList<String>():user.getKeyWords())
+                    );
+        }
+        catch (JsonProcessingException ignore){
+
+        }
+        return null;
     }
 
     @Override
     public Mono<User> findById(UUID uuid) {
         return userDao.findById(uuid);
+    }
+
+    @Override
+    public Mono<Boolean> existUserByName(String username) {
+        return userDao.exists(Example.of(User.builder().username(username).build()));
     }
 }
